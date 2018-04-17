8dc0ff9798967ea1500ac49e3ca10e4a19d89f0a
Jasper <36561872+agapius@users.noreply.github.com>
2018-04-17 09:28:21 +0200

    Cleaner Ethercow
    Cleaned up the code and implemented Coinmarketcap Api.

--- ethercow.py
+++ ethercow.py
@@ -1,29 +1,19 @@
 import requests
 import json
 
-##### get the JSON code of the site #####
-req1 = requests.get("https://www.bitstamp.net/api/v2/ticker/btceur/")
-req2 = requests.get("https://www.bitstamp.net/api/v2/ticker/etheur/")
+###### get the Info from Coinmarketcap Api ######
 
-content1 = req1.content.decode("utf-8")
-content2 = req2.content.decode("utf-8")
+ethereum_dic = requests.get("https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR").json()[0]
 
-#########################################
+eth_price=(ethereum_dic.get("price_eur"))
 
-loaded1 = json.loads(content1)
-loaded2 = json.loads(content2)
-
-btc = loaded1['last']
-eth = loaded2['last']
-
-#########################################
-
+###### get the User's Info ######
 
 amount_eth = 100
 
-output_wealth = (float(eth)*amount_eth)
+output_wealth = (float(eth_price)*amount_eth)
 
-#########################################
+###### print ASCII Art Ethercow ######
 print()
 print("     /----------\\") 
 print("   < ", round(output_wealth,2), "€", " >")
